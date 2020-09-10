f05cc7be6f8dfe4d4e6a8fa80fc652bb0594d7f2
Pedro <pedro-gomes17@hotmail.com>
2020-09-10 13:20:49 -0300

    Added new route components: Home and Matches

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,16 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-
-// import Route from './Route';
+import Home from '../pages/Home';
+import Matches from '../pages/Matches';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" exact component={SignUp} />
+      <Route path="/home" exact component={Home} />
+      <Route path="/matches" exact component={Matches} />
     </Switch>
   </BrowserRouter>
 );
