9e175580eee26a10d156a39118ee4e24f094dcf1
Pedro <pedro-gomes17@hotmail.com>
2020-10-09 12:03:46 -0300

    Changed page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,224 +1,37 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable camelcase */
-import React, { useState, useEffect } from 'react';
-import { toast } from 'react-toastify';
-import { useForm } from 'react-hook-form';
-import { useLocation } from 'react-router-dom';
-import { MdDeleteForever } from 'react-icons/md';
-import Header from '../../components/Header';
-import Input from '../../components/Input';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Button from '../../components/Button';
-import Modal from '../../components/Modal';
-import {
-  Container,
-  Table,
-  WrapperForm,
-  WrapperPlayersPerTeam,
-  WrapperRegisterPlayers,
-} from './styles';
-import api from '../../services/api';
-
-interface FormDataProps {
-  id: number;
-  name: string;
-  year: number;
-  team: {
-    id: number;
-    name: string;
-  };
-  position: string;
-}
-
-interface FormDataProps2 {
-  quantity: number;
-}
-
-interface PropsParam {
-  match_id: number;
-}
+import ModalRegisterMatch from '../../components/ModalRegisterMatch';
+import Header from '../../components/Header';
+import { Container, WrapperButtons } from './styles';
 
 const Home: React.FC = () => {
-  const { state } = useLocation<PropsParam>();
-
-  const [matchId] = useState(state.match_id);
-  const [tableData, setTableData] = useState<FormDataProps[]>([]);
+  const history = useHistory();
   const [modalOpen, setModalOpen] = useState(false);
-  const [updateTable, setUpdateTable] = useState(true);
-  const [goalkeeper, setIsGoalkeeper] = useState(false);
-  const [, setPlayersPerTeam] = useState(0);
-  const [visibleFormPlayers, setVisibleFormPlayers] = useState(1);
-  const [confirmPlayersPerTeam, setConfirmPlayersPerTeam] = useState(0);
-  const { register, handleSubmit } = useForm<FormDataProps>();
-  const {
-    register: registerQuantity,
-    handleSubmit: handleSubmitQuantity,
-  } = useForm<FormDataProps2>();
-  useEffect(() => {
-    async function loadPlayers(): Promise<void> {
-      if (updateTable === true) {
-        const response = await api.get('/players');
-
-        setTableData(response.data);
-        setUpdateTable(false);
-      }
-    }
-    loadPlayers();
-  }, [updateTable]);
-
-  const registerPlayersPerTeam = async (data: any): Promise<void> => {
-    const randomTeam = Math.random() * (4 - 1) + 1;
-
-    try {
-      await api.post('/players', {
-        name: data.name,
-        year: data.year,
-        position: data.position,
-        team_id: Math.round(randomTeam),
-        matches_id: matchId,
-        // isgoalkeeper: data.isgoalkeeper,
-      });
-      setUpdateTable(true);
-
-      toast.success('Jogador cadastrado com sucesso!');
-    } catch (error) {
-      if (error.response.data.error.includes('O limite de jogadores do time')) {
-        toast.info(`${error.response.data.error}`);
-      } else {
-        toast.error('Erro ao cadastrar');
-      }
-    }
-  };
-
-  const updateQuantityPlayers = async (data: any): Promise<void> => {
-    try {
-      await api.put('/teams', {
-        quantity: data.quantity,
-      });
-
-      setConfirmPlayersPerTeam(1);
-      setVisibleFormPlayers(0);
-    } catch (error) {
-      toast.error('Erro ao atualizar a quantidade por time');
-    }
-  };
-
-  const handleDelete = async (id: number): Promise<void> => {
-    try {
-      await api.delete(`/players/${id}`);
-
-      toast.success('Jogador deletado com sucesso!');
-
-      setTableData(tableData.filter(element => element.id !== id));
-    } catch (error) {
-      toast.error('Erro ao deletar jogador');
-    }
-  };
 
   return (
-    <Container>
+    <>
       <Header />
+      <Container>
+        <h1>Seja bem vindo ao #VemProFut.</h1>
+        <strong>Selecione a opção desejada:</strong>
 
-      <main>
-        <Table>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Idade</th>
-              <th>Time</th>
-              <th>Posição</th>
-              <th>Excluir</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData.map(item => (
-              <tr key={item.id}>
-                <td>{item.name}</td>
-                <td>{item.year}</td>
-                <td>{item.team.name}</td>
-                <td>{item.position}</td>
-                <td className="delete" onClick={() => handleDelete(item.id)}>
-                  <MdDeleteForever color="#ff0015" size={20} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        <WrapperButtons>
+          <Button onClick={() => setModalOpen(true)}>Cadastrar novo</Button>
 
-        <WrapperForm>
-          <WrapperPlayersPerTeam playersPerTeam={confirmPlayersPerTeam}>
-            <form
-              onSubmit={handleSubmitQuantity(updateQuantityPlayers)}
-              key={2}
-            >
-              <label htmlFor="name">Quantidade de jogadores por time:</label>
-              <select
-                name="quantity"
-                onChange={e => setPlayersPerTeam(Number(e.target.value))}
-                ref={registerQuantity}
-              >
-                <option value="">Escolher</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-              </select>
-              <Button type="submit">Confirmar</Button>
-            </form>
-          </WrapperPlayersPerTeam>
+          <Button onClick={() => history.push('/list-matches')}>
+            Listar peladas cadastradas
+          </Button>
+        </WrapperButtons>
 
-          <WrapperRegisterPlayers visibleFormPlayers={visibleFormPlayers}>
-            <form onSubmit={handleSubmit(registerPlayersPerTeam)} key={1}>
-              <h2>Cadastro de jogadores </h2>
-              <label htmlFor="name">Nome/Apelido:</label>
-              <Input
-                id="name"
-                name="name"
-                type="name"
-                register={register}
-                required
-              />
-              <label htmlFor="year">Idade:</label>
-              <Input
-                id="year"
-                name="year"
-                type="number"
-                label="Idade"
-                register={register}
-                required
-              />
-
-              <label htmlFor="position">Posição:</label>
-              <select
-                name="position"
-                id="position"
-                disabled={goalkeeper}
-                ref={register}
-                required
-              >
-                <option value="Goleiro"> Goleiro </option>
-                <option value="Zagueiro"> Zagueiro </option>
-                <option value="Meio de campo"> Meio de campo </option>
-                <option value="Atacante"> Atacante </option>
-              </select>
-
-              <Button type="submit">Inserir jogador</Button>
-              <Button type="button" onClick={() => setModalOpen(true)}>
-                Sortear times
-              </Button>
-            </form>
-          </WrapperRegisterPlayers>
-        </WrapperForm>
-      </main>
-
-      {modalOpen && (
-        <Modal onClose={() => setModalOpen(false)} open={() => {}} />
-      )}
-    </Container>
+        {modalOpen && (
+          <ModalRegisterMatch
+            onClose={() => setModalOpen(false)}
+            open={() => {}}
+          />
+        )}
+      </Container>
+    </>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,150 +1,31 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-interface IPlayersPerTeam {
-  playersPerTeam: number;
-}
-interface IVisibleFormPlayers {
-  visibleFormPlayers: number;
-}
 export const Container = styled.div`
-  main {
-    margin: 0 auto;
-    max-width: 125rem;
-
-    margin-top: 3.5rem;
-
-    display: flex;
-  }
-`;
-export const Table = styled.table`
-  width: 50%;
-  border-collapse: collapse;
-  td,
-  th {
-    text-align: center;
-    padding: 1rem;
-    border: 1px solid ${({ theme }) => theme.colors.quaternary};
-  }
-
-  .delete {
-    cursor: pointer;
-    transition: 0.3s;
-
-    &:hover {
-      opacity: 0.7;
-    }
-  }
-`;
-export const WrapperForm = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
-  flex: 1;
-  margin-left: 5rem;
+
+  max-width: 125rem;
+
+  margin: 0 auto;
+  margin-top: 8rem;
+
+  h1 {
+    max-width: 50rem;
+    margin-top: 3rem;
+  }
+
+  strong {
+    margin-top: 1rem;
+  }
 `;
 
-export const WrapperPlayersPerTeam = styled.div<IPlayersPerTeam>`
+export const WrapperButtons = styled.div`
   display: flex;
   flex-direction: row;
-  margin: 0 auto 2rem;
-
-  cursor: ${({ playersPerTeam }) =>
-    playersPerTeam > 0 ? 'not-allowed' : 'auto'};
-
-  opacity: ${({ playersPerTeam }) => (playersPerTeam > 0 ? '0.2' : '1')};
-
-  select,
-  button {
-    pointer-events: ${({ playersPerTeam }) =>
-      playersPerTeam > 0 ? 'none' : 'auto'};
-  }
-
-  select {
-    height: 3rem;
-    border-radius: 0.5rem;
-    border: 0.17rem solid ${({ theme }) => theme.colors.secondary};
-  }
-
-  label {
-    margin-right: 0.5rem;
-  }
 
   button {
-    margin: 2rem auto 0;
-    width: 50%;
-    height: 4rem;
-  }
-
-  form {
-    border-radius: 0.5rem;
-    padding: 2rem;
-    border: 0.17rem solid ${({ theme }) => theme.colors.quaternary};
-  }
-`;
-
-export const WrapperRegisterPlayers = styled.div<IVisibleFormPlayers>`
-  width: 100%;
-
-  cursor: ${({ visibleFormPlayers }) =>
-    visibleFormPlayers > 0 ? 'not-allowed' : 'auto'};
-
-  input,
-  select,
-  button {
-    pointer-events: ${({ visibleFormPlayers }) =>
-      visibleFormPlayers > 0 ? 'none' : 'auto'};
-  }
-
-  opacity: ${({ visibleFormPlayers }) =>
-    visibleFormPlayers > 0 ? '0.2' : '1'};
-
-  > form {
-    display: flex;
-    flex-direction: column;
-
-    width: 100%;
-
-    border: 1px solid ${({ theme }) => theme.colors.quaternary};
-    border-radius: 0.5rem;
-
-    padding: 2rem;
-
-    select {
-      width: 30%;
-      height: 3rem;
-      margin-bottom: 1rem;
-      border-radius: 0.5rem;
-      border: 0.17rem solid ${({ theme }) => theme.colors.secondary};
-    }
-
-    button {
-      width: 50%;
-      margin: 1rem auto;
-    }
-
-    label {
-      margin-bottom: 0.5rem;
-    }
-
-    h2 {
-      text-align: center;
-      margin-bottom: 1rem;
-    }
-
-    > div {
-      display: flex;
-      align-items: center;
-      margin-bottom: 2rem;
-
-      label {
-        margin-bottom: 0;
-        margin-left: 0.5rem;
-      }
-
-      input [type='checkbox'] {
-        width: 1.5rem;
-        height: 1.5rem;
-      }
-    }
+    width: 15rem;
+    height: 10rem;
+    margin-right: 1rem;
   }
 `;
